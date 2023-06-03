8d45942dd0c9a0e3fb7ca35d904e57317b5b149d
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-06-03 12:08:40 -0400

    added apple stock

---
+++ app/seeds/stocks.py
@@ -0,0 +1,36 @@
+from app.models import db, Stock, environment, SCHEMA
+from sqlalchemy.sql import text
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_stocks():
+    apple = Stock(
+        name = “Apple Inc”,
+        symbol = “AAPL”,
+        description = “Apple Inc. is an American multinational technology company that specializes in consumer electronics, computer software, and online services. Apple is the world's largest technology company by revenue (totalling $274.5 billion in 2020) and, since January 2021, the world's most valuable company. As of 2021, Apple is the world's fourth-largest PC vendor by unit sales, and fourth-largest smartphone manufacturer. It is one of the Big Five American information technology companies, along with Amazon, Google, Microsoft, and Facebook.”,
+        market_cap = 2788699000000,
+        pe_ratio = 29.0,
+        dividend = .0054,
+        stock_ceo = “Tim Cook”,
+        employees = 147000,
+        headquarters = “Cupertino, California”,
+        year_founded = 1976
+    )
+
+
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_stocks():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM stocks"))
+
+    db.session.commit()
