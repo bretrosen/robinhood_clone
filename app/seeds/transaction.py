32472bc993315f2c1011d53ec51d4744b83fb444
Bret Rosen <bretrosen@gmail.com>
2023-06-03 11:47:00 -0500

    added random faker functions for transaction seeds

--- app/seeds/transaction.py
+++ app/seeds/transaction.py
@@ -1,7 +1,51 @@
 from sqlalchemy.sql import text
 from app.models import db, Transaction, environment, SCHEMA
+from random import randint, choice
+from faker import Faker
+
+fake = Faker()
+
+def generate_transactions():
+    transactions = []
+    # Want transaction data for all stocks
+    for stock_id in range(1, 21):
+        # Seed 20 transactions per stock
+        for _ in range(20):
+            # Randomly choose a user for a transaction
+            user_id = randint(1, 3)
+            # Randomly choose a quantity between 1 and 10
+            quantity = fake.random.uniform(1, 10)
+            # Randomly choose a buy or sell for transaction
+            purchased = choice[True, False]
+            if purchased:
+                price_purchased = fake.random.uniform(1, 100)
+            else:
+                price_sold = fake.random.uniform(1, 100)
+            new_transaction = {
+                'stock_id': stock_id,
+                'user_id': user_id,
+                'quantity': quantity,
+                'purchased': purchased,
+                'price_purchased': price_purchased,
+                'price_sold': price_sold
+            }
+            transactions.append(Transaction(**new_transaction))
+    return transactions
+
 
 def seed_transaction():
+    transaction_list = generate_transactions()
+    for transaction in transaction_list:
+        value = transaction.to_dict()
+        db.session.add(Transaction( stock_id = value['stock_id'],
+                                   user_id = value['user_id'],
+                                   quantity = value['quantity'],
+                                   purchased = value['purchased'],
+                                   price_purchased = value['price_purchased'],
+                                   price_sold = value['price_sold']
+        ))
+    db.session.commit()
+
     transaction1 = Transaction(stock_id=1,
                                user_id=1,
                                 quantity=1,
