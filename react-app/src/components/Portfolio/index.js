757d9cb2b6d54926dd9923f01613e75b85cff06a
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-06-04 14:13:51 -0400

    able to render each watch list

--- react-app/src/components/Portfolio/index.js
+++ react-app/src/components/Portfolio/index.js
@@ -5,11 +5,18 @@ import { fetchPortfolio } from "../../store/user";
 export default function Portfolio() {
     const { user } = useSelector(state => state)
     const sessionUser = useSelector(state => state.session.user);
-    console.log(sessionUser);
+    // console.log(sessionUser);
+    const watchlists = user.watch_lists
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(fetchPortfolio(sessionUser.id))
     }, [dispatch, sessionUser.id])
+
+    if (!watchlists) {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
     return (
         <div className="portfolio-page">
             <div className="portfolio">
@@ -19,9 +26,9 @@ export default function Portfolio() {
                 <div className="transfer">
                     <div className="tranfer-left">
 
-                    <span>Application in review</span>
-                    <p>Start a transfer so you can invest as soon as your application is approved.</p>
-                    <p className="login-signup">Transer money</p>
+                        <span>Application in review</span>
+                        <p>Start a transfer so you can invest as soon as your application is approved.</p>
+                        <p className="login-signup">Transer money</p>
                     </div>
                     <img src="/static/phone-money.svg" alt="money coming out of phone"></img>
                 </div>
@@ -31,11 +38,21 @@ export default function Portfolio() {
                     <p>Lists</p>
                     <p>+</p>
                 </div>
-                <div className="watchlist">
-                    <span>⚡️</span>
-                    <p>My First List</p>
-                    
-                </div>
+                {watchlists.map((list,  index) => {
+
+                    return (
+                    <div className="watchlist" key={`watchlist-index-${index}`}>
+                        <div className="watchlist-left">
+                            <span>⚡️</span>
+                                <p>{list.name}</p>
+                        </div>
+                        <div>
+                            <i className="fa fa-ellipsis-h"></i>
+                            <span>^</span>
+                        </div>
+                    </div>)
+
+                })}
             </div>
         </div>
     )
