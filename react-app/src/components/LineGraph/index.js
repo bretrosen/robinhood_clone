b41ac68e9e44a970b23bf8ec571113d9fbcc9223
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-06-05 11:31:16 -0400

    added example chart to porfolio page

---
+++ react-app/src/components/LineGraph/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+const LineChart = () => {
+    const data = {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+        datasets: [
+            {
+                label: 'Data 1',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            },
+            {
+                label: 'Data 2',
+                data: [5, 8, 2, 4, 9, 6],
+                backgroundColor: 'rgba(192, 75, 192, 0.2)',
+                borderColor: 'rgba(192, 75, 192, 1)',
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    };
+
+    return (
+        <div>
+            <h2>Line Chart Example</h2>
+            <Line data={data} options={options} />
+        </div>
+    );
+};
+
+export default LineChart;
--- react-app/src/components/Portfolio/index.js
+++ react-app/src/components/Portfolio/index.js
@@ -1,7 +1,9 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchPortfolio } from "../../store/user";
+
 import "./portfolio.css"
+import LineChart from "../LineGraph";
 export default function Portfolio() {
     const { user } = useSelector(state => state)
     const sessionUser = useSelector(state => state.session.user);
@@ -22,7 +24,7 @@ export default function Portfolio() {
             <div className="portfolio">
                 <img src="/static/stock-dice.svg" alt="dice with stocks on the sides" style={{ width: "100%" }}></img>
                 <h1>Welcome to Foxtrot</h1>
-
+                <LineChart />
                 <div className="transfer">
                     <div className="transfer-left">
 
