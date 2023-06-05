f68b727a309d30725de251d6315e96bb008638ca
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-06-05 16:14:35 -0400

    made post watchlist route

---
+++ app/api/watchlist_routes.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from app.models import db, WatchList
+from app.forms.create_watchlist_form import WatchlistFormCreate
+
+
+watchlist_routes = Blueprint('watchlists', __name__)
+
+@watchlist_routes.route("", methods = ["POST"])
+def create_watchlist():
+    form = WatchlistFormCreate()
+    user = current_user
+    form['csrf_token'].data = request.cookies['csrf_token'] # Boilerplate code
+
+    if form.validate_on_submit():
+        new_watchlist = WatchList(
+            name = form.data["name"],
+            user_id = user.id
+        )
+        db.session.add(new_watchlist)
+        db.session.commit()
+        print('added new watchlist')
+        return new_watchlist.to_dict()
+
+    if form.errors:
+        print(form.errors)
+        return form.errors
