64d0a62ac6704b6288ef50a4eb29071615046351
Bret Rosen <bretrosen@gmail.com>
2023-06-06 15:06:29 -0500

    merging, moving thunks from stock to user reducer

--- react-app/src/store/stock.js
+++ react-app/src/store/stock.js
@@ -1,8 +1,5 @@
 // action type constants
-
 const GET_STOCK = '/stock/GET_STOCK'
-const BUY_STOCK = '/stock/BUY_STOCK'
-const SELL_STOCK = '/stock/SELL_STOCK'
 const ALL_STOCKS = '/stocks/ALL_STOCKS'
 
 
@@ -14,6 +11,12 @@ const stockDetail = (stock) => {
     }
 }
 
+const allStocks = (stocks) => {
+    return {
+        type: ALL_STOCKS,
+        stocks
+    }
+}
 
 
 // thunk action creators
@@ -38,48 +41,17 @@ export const stockDetailsThunk = (stockId) => async (dispatch) => {
     }
 }
 
+export const fetchAllStocks = () => async (dispatch) => {
+    const response = await fetch(`/api/stocks/`)
 
     if (response.ok) {
-        const stockBought = await response.json();
-        console.log('returning buy stock thunk', stockBought)
-        dispatch(buyStock(stockBought))
-    } else if (response.status < 500) {
-        console.log("response status", response.status)
-        const data = await response.json();
-        if (data.errors) {
-            return data.errors;
-        }
-    } else {
-        console.log("An error occured")
-        return ["An error occurred. Please try again."];
+        const stocks = await response.json()
+        dispatch(allStocks(stocks))
     }
 }
 
-export const sellStockThunk = (stock) => async (dispatch) => {
-    const response = await fetch(`/api/stocks/${stock.id}/sell_stock`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(stock)
-    })
-    console.log('sending sell stock thunk', response)
 
-    if (response.ok) {
-        const stockSold = await response.json();
-        console.log('returning sell stock thunk', stockSold)
-        dispatch(sellStock(stockSold))
-    } else if (response.status < 500) {
-        console.log("response status", response.status)
-        const data = await response.json();
-        if (data.errors) {
-            return data.errors;
-        }
-    } else {
-        console.log("An error occured")
-        return ["An error occurred. Please try again."];
-    }
-}
-
-const initialState = { stock: null, user: null}
+const initialState = { stock: null }
 
 // reducer
 export default function stockReducer(state = initialState, action) {
@@ -88,11 +60,6 @@ export default function stockReducer(state = initialState, action) {
 
     switch (action.type) {
         case GET_STOCK:
-            return { ...action.stock };
-
-        case BUY_STOCK:
-            return { ...action.stock };
-        case SELL_STOCK:
             return { ...action.stock };
         case ALL_STOCKS:
 
