275f6c9409bf6257210b0f6477f4fa70d5c256db
Bret Rosen <bretrosen@gmail.com>
2023-06-06 13:23:16 -0500

    buy and sell almost working

--- react-app/src/components/Transaction/index.js
+++ react-app/src/components/Transaction/index.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { buyStockThunk, sellStockThunk } from '../../store/stock'
+import { buyStockThunk, sellStockThunk } from '../../store/user'
 
 export const BuySomeStock = () => {
 
@@ -22,7 +22,6 @@ export const BuySomeStock = () => {
         await dispatch(buyStockThunk(stockObj))
     }
 
-
     return (
         <div>
             <div>Buy {stock.symbol} </div>
@@ -31,6 +30,40 @@ export const BuySomeStock = () => {
                     value={quantity}
                     onChange={e => setQuantity(e.target.value)} />
                 <button>Buy Stock</button>
+                </form>
+        </div>
+    )
+
+}
+
+export const SellSomeStock = () => {
+
+    const [quantity, setQuantity] = useState(0)
+    const dispatch = useDispatch()
+    const stock = useSelector(state => state.stock)
+    const marketPrice = stock.stock_history[0].price
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        // object to match the thunk and backend buy route
+        // needs id, quantity, price_purchased
+        const stockObj = {
+            "id": stock.id,
+            // convert to number with 2 decimal points
+            "quantity": Number.parseFloat(quantity).toFixed(2),
+            "price_sold": marketPrice
+        }
+        console.log("dispatching the sell stock thunk from form =>", stockObj)
+        await dispatch(sellStockThunk(stockObj))
+    }
+
+    return (
+        <div>
+            <div>Sell {stock.symbol} </div>
+            <form onSubmit={handleSubmit}>
+                <input placeholder="Quantity"
+                    value={quantity}
+                    onChange={e => setQuantity(e.target.value)} />
+                <button>Sell Stock</button>
                 </form>
         </div>
     )
--- react-app/src/store/stock.js
+++ react-app/src/store/stock.js
@@ -1,9 +1,7 @@
-// action type constants
+// action type constant
 const GET_STOCK = 'stock/GET_STOCK'
-const BUY_STOCK = 'stock/BUY_STOCK'
-const SELL_STOCK = 'stock/SELL_STOCK'
 
-// action creators
+// action creator
 const stockDetail = (stock) => {
     return {
         type: GET_STOCK,
@@ -11,21 +9,9 @@ const stockDetail = (stock) => {
     }
 }
 
-const buyStock = (stock) => {
-    return {
-        type: BUY_STOCK,
-        stock
-    }
-}
 
-const sellStock = (stock) => {
-    return {
-        type: SELL_STOCK,
-        stock
-    }
-}
 
-// thunk action creators
+// thunk action creator
 export const stockDetailsThunk = (stockId) => async (dispatch) => {
     const response = await fetch(`/api/stocks/${stockId}`)
     console.log('sending stock details thunk')
@@ -47,56 +33,8 @@ export const stockDetailsThunk = (stockId) => async (dispatch) => {
     }
 }
 
-export const buyStockThunk = (stock) => async (dispatch) => {
-    const response = await fetch(`/api/stocks/${stock.id}/buy_stock`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({"quantity": stock.quantity,
-               "price_purchased": stock.price_purchased})
-    })
-    console.log('sending buy stock thunk', response)
 
-    if (response.ok) {
-        const stockBought = await response.json();
-        console.log('returning buy stock thunk', stockBought)
-        dispatch(buyStock(stockBought))
-    } else if (response.status < 500) {
-        console.log("response status", response.status)
-        const data = await response.json();
-        if (data.errors) {
-            return data.errors;
-        }
-    } else {
-        console.log("An error occured")
-        return ["An error occurred. Please try again."];
-    }
-}
-
-export const sellStockThunk = (stock) => async (dispatch) => {
-    const response = await fetch(`/api/stocks/${stock.id}/sell_stock`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(stock)
-    })
-    console.log('sending sell stock thunk', response)
-
-    if (response.ok) {
-        const stockSold = await response.json();
-        console.log('returning sell stock thunk', stockSold)
-        dispatch(sellStock(stockSold))
-    } else if (response.status < 500) {
-        console.log("response status", response.status)
-        const data = await response.json();
-        if (data.errors) {
-            return data.errors;
-        }
-    } else {
-        console.log("An error occured")
-        return ["An error occurred. Please try again."];
-    }
-}
-
-const initialState = { stock: null, user: null}
+const initialState = { stock: null }
 
 // reducer
 export default function stockReducer(state = initialState, action) {
