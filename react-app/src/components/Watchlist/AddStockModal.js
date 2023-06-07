125a16dfea52c34011a79458ef0e640e121c9304
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-06-07 11:31:06 -0400

    edit

--- react-app/src/components/Watchlist/AddStockModal.js
+++ react-app/src/components/Watchlist/AddStockModal.js
@@ -2,6 +2,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { addStockToWatchlist, fetchPortfolio } from '../../store/user';
+import { useModal } from '../../context/Modal';
 
 export default function AddStockModal({stock}) {
     const { user } = useSelector(state => state)
@@ -9,6 +10,7 @@ export default function AddStockModal({stock}) {
     const [checkedLists, setCheckedLists] = useState([]);
     const watchlists = user.watch_lists
     const dispatch = useDispatch();
+    const { closeModal } = useModal();
 
     useEffect(() => {
         console.log("right before dispatch");
@@ -26,6 +28,7 @@ export default function AddStockModal({stock}) {
             setCheckedLists(prevState => prevState.filter(listId => listId !== id));
         } else {
             setCheckedLists(prevState => [...prevState, id]);
+
         }
     };
 
@@ -33,7 +36,8 @@ export default function AddStockModal({stock}) {
     // console.log(stock);
     const saveChanges = () => {
         console.log(checkedLists);
-        dispatch(addStockToWatchlist(checkedLists, stock.id ))
+        dispatch(addStockToWatchlist(checkedLists, stock.id))
+        closeModal()
     }
     return (
         <div className="portfolio-watchlist lists-modal">
