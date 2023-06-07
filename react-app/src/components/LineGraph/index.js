794b9c6c35959cb51c080f5190d4965f624a4bb4
snydernb1 <snydernb1@gmail.com>
2023-06-07 14:38:21 -0600

    Created function that displays user portfolio value for single stock

--- react-app/src/components/LineGraph/index.js
+++ react-app/src/components/LineGraph/index.js
@@ -8,29 +8,34 @@ const LineChart = () => {
     const {stock} = useSelector(state => state.stock)
     const dispatch = useDispatch()
 
-    console.log('Is this apple?', stock.stock_history)
+    // console.log('Is this apple?', stock.stock_history)
 
     useEffect(()=> {
         dispatch(stockDetailsThunk(1))
     }, [dispatch])
 
-    const findStockAmount = (user, stockId) => {
+    // console.log('what is stock', Object.values(stock))
+    if (Object.values(stock).length === 0) return false
+
+    const findStockValue = (user, stockId) => {
         const transactions = user.transactions
-        let resStockTransactions = []
-        let totalQuantity = 0
-        let earliestDate = 0;
+        let filterStockTransactions = []
+        let totalQuantity = 0;
         let initialValue = 0;
+        let earliestDate = 0;
         const stockValArr = [];
-        const stockDateArr = [];
+
+
+        let filterHistory = [];
 
         // Sorting through users transactions and filtering down to a single stock
         for (let stock of transactions) {
             if (stock.stock_id === stockId) {
-                resStockTransactions.push(stock)
+                filterStockTransactions.push(stock)
             }
         }
         // From the filtered list, calulating earliest date, total quantity of stock and initial value
-        for (let item of resStockTransactions) {
+        for (let item of filterStockTransactions) {
             if (item.purchased === true) {
                 totalQuantity += item.quantity
                 initialValue += item.quantity * item.price_purchased
@@ -48,16 +53,81 @@ const LineChart = () => {
         }
 
         stockValArr.push(initialValue)
-        stockDateArr.push(earliestDate)
-        console.log('Is the quantity calculating?', totalQuantity)
-        console.log('Is the initial value calculating?', initialValue)
-        console.log('Is the earliest date calculating?', earliestDate)
+        // console.log('Is the quantity calculating?', totalQuantity)
+        // console.log('Is the initial value calculating?', initialValue)
+        // console.log('Is the earliest date calculating?', earliestDate)
 
+        // Filters stock history to items after users initial buy date
+        for (let item of stock.stock_history) {
+            let date = Date.parse(item.time_stamp)
+            if (date > earliestDate) {
+                filterHistory.push(item)
+            }
+        }
 
+        for (let i = filterHistory.length - 1; i >= 0; i--) {
+            let item = filterHistory[i]
+            stockValArr.push(totalQuantity * item?.price)
+        }
+
+        // console.log('This should be filtered history', filterHistory)
+        // console.log('This should be stock value array', stockValArr)
+        return stockValArr
 
     }
 
-    findStockAmount(user, 1)
+    const findStockDates = (user, stockId) => {
+        const transactions = user.transactions
+        let filterStockTransactions = []
+        let earliestDate = 0;
+        const stockDateArr = [];
+
+        let filterHistory = [];
+
+        // Sorting through users transactions and filtering down to a single stock
+        for (let stock of transactions) {
+            if (stock.stock_id === stockId) {
+                filterStockTransactions.push(stock)
+            }
+        }
+        // From the filtered list, calulating earliest date, total quantity of stock and initial value
+        for (let item of filterStockTransactions) {
+            let date = Date.parse(item.time_stamp)
+            if (earliestDate === 0) {
+                earliestDate = date
+            } else if (date < earliestDate) {
+                earliestDate = date
+            }
+        }
+
+        stockDateArr.push(earliestDate)
+
+        // Filters stock history to items after users initial buy date
+        for (let item of stock.stock_history) {
+            let date = Date.parse(item.time_stamp)
+            if (date > earliestDate) {
+                filterHistory.push(item)
+            }
+        }
+
+        for (let i = filterHistory.length - 1; i >= 0; i--) {
+            let item = filterHistory[i]
+            stockDateArr.push(Date.parse(item?.time_stamp))
+        }
+
+        let resDateArr = [];
+
+        for (let date of stockDateArr) {
+            let dateFormat = new Date(date)
+            resDateArr.push(dateFormat.getDate() + '/' + dateFormat.getMonth() + '/' + dateFormat.getFullYear())
+        }
+        console.log('Should be all dates', resDateArr)
+        return resDateArr
+    }
+
+
+    const vals = findStockValue(user, 1)
+    const dates = findStockDates(user, 1)
 
 
 
@@ -65,20 +135,25 @@ const LineChart = () => {
 
 
     const data = {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+        labels: dates,
         datasets: [
             {
-                label: 'Data 1',
-                data: [12, 19, 3, 5, 2, 3],
+                label: 'Value',
+                data: findStockValue(user, 1),
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                borderColor: 'rgba(75, 192, 192, 1)',
+                borderColor: vals[0] < vals[vals.length - 1] ? 'green' : 'red',
                 borderWidth: 1,
             }
         ],
     };
 
     const options = {
+        plugins: {
+            legend: {display: false},
+            tooltip: {intersect: true}
+        },
         responsive: true,
+
         scales: {
             y: {
                 beginAtZero: true,
@@ -90,6 +165,7 @@ const LineChart = () => {
         <div>
             <h2>Line Chart Example</h2>
             <Line data={data} options={options} />
+
         </div>
     );
 };
