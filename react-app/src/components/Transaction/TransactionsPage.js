9c8d2f4d304edbc6d7773ad30ff157c6dc1a49b1
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-06-08 15:50:31 -0400

    transactions

--- react-app/src/components/Transaction/TransactionsPage.js
+++ react-app/src/components/Transaction/TransactionsPage.js
@@ -1,11 +1,54 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchPortfolio } from "../../store/user";
+import { useEffect } from "react";
 
 export default function TransactionsPage(){
-    const {user} = useSelector(state => state)
-
+    const { user } = useSelector(state => state)
+    const sessionUser = useSelector(state => state.session.user);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchPortfolio(sessionUser.id))
+    }, [dispatch, sessionUser.id])
+    if (!user) {
+        return (
+            <div>
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
+    const transactions = user.transactions
+    console.log(transactions);
     return (
-        <div>
-            TransactionsPage
+        <div className="transactions-page">
+            <div className="portfolio-page">
+
+                <table id="watchlist-table">
+                    <thead>
+                        <tr>
+                            <th>Stock</th>
+                            <th>Cost</th>
+                            <th>Quantity</th>
+                            <th>Date</th>
+                            <th>Market Cap</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {transactions?.map((transaction, index) => {
+                            return (<tr className="table-row" key={`transaction-list-${index}`} >
+                                <td>{transaction.stock_id}</td>
+                                <td>${(transaction.quantity * transaction.price_purchased).toFixed(2)}</td>
+                                <td>$15.75</td>
+                                <td>+0.8%</td>
+                                <td>{}</td>
+                                <td className="delete-transaction"><i className="fa fa-trash"></i></td>
+                            </tr>)
+
+                        })}
+                    </tbody>
+                </table>
+
+            </div>
         </div>
     )
 }
